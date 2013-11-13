87e8c320753a7cc5686302cc4652b71728b233e9
Scott Byrns <scott@scotts-MacBook-Pro.local>
2013-11-13 13:13:52 -0700

    Roughtly pieced together from JDM Repo Manager

---
+++ src/widget-builder.js
@@ -0,0 +1,23 @@
+LiveWidgets.addWidget({
+	
+
+	name: "widget-builder",
+	
+	model: {
+		
+	},
+	
+	controller: {
+		
+	},
+	
+	constructor: function () {
+		
+	},
+	
+	reinit: function () {
+		
+	}
+	
+	
+});
